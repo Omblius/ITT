a4a7640ec7f715fbb999c057f24a3eecf67c4bc8
student <student@MININT-5L2GJDQ.campus.ittesi.com>
2015-06-22 20:12:53 -0700

    In-class commit.

---
+++ ITTMaster/src/com/itt/ifthenelse/IfThenElse.java
@@ -0,0 +1,15 @@
+package com.itt.ifthenelse;
+
+public class IfThenElse {
+	static int x = 30;
+
+	public static void main(String[] args) {
+		if (x < 20) {
+			System.out.println("Yes, x is less than 20.");
+		} else {
+			System.out.println("No, x is not less than 20.");
+		}
+
+	}
+
+}
